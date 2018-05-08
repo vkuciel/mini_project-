9cc99b92fa7ec8f56cb63187dd07a164cd6bba88
Kevin <Kevin@10.0.0.245>
2018-05-08 01:36:59 -0400

    working on a way to check for player wins

--- ttt.c
+++ ttt.c
@@ -116,3 +116,30 @@ void print_board() {
     }
     printf("\n\n");
 }
+
+
+
+//int win_check()
+//{
+//    int i, j;
+//    for(i=0; i<N; i++)  //check rows
+//        if(board[i][0]==board[i][1] && board[i][0]==board[i][2]){
+//            return board[i][0];
+//        }
+//
+//    for(j=0; j<N; j++)   // check columns
+//        if(board[0][i]==board[1][i] && board[0][i]==board[2][i]){
+//            return board[0][i];
+//         }
+//
+//    // check diagonals
+//    if(board[0][0]==board[1][1] &&  board[1][1]==board[2][2]){
+//        return board[0][0];
+//     }
+//
+//    if(board[0][2]==board[1][1] && board[1][1]==board[2][0]){
+//        return board[0][2];
+//    }
+//
+//    return 0;
+//}
